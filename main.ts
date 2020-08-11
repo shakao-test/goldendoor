b4b2be8563773604e0fa74d065ae6d3355bd926e
Kiki <kiki-lee@users.noreply.github.com>
2020-08-10 18:12:35 -0700

    Remove Starter Code

--- main.ts
+++ main.ts
@@ -1,9 +1,1 @@
-input.onPinPressed(TouchPin.P0, function () {
-    custom.checkDoor(0)
-})
-input.onPinPressed(TouchPin.P2, function () {
-    custom.checkDoor(2)
-})
-input.onPinPressed(TouchPin.P1, function () {
-    custom.checkDoor(1)
-})
+
